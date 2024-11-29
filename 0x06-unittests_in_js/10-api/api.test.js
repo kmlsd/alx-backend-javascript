3c046c64f87ace3f6acab3c05def6af3c714ed01
kmlsd <kmlsd4757@gmail.com>
2024-11-29 19:51:06 +0000

    unittests_in_js

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
+
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.equal("Welcome to the payment system");
+	    done();
+	});
+    });
+});
